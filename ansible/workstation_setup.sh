70205b5504698c1c212c317f80251a417a6e5a33
Juan Lazarde <juanlazarde@gmail.com>
2022-02-09 17:14:12 -0500

    removed the -C flag from workstation_setup.sh

--- ansible/workstation_setup.sh
+++ ansible/workstation_setup.sh
@@ -1,1 +1,1 @@
-ansible-playbook playbook.yml -i hosts.yml --ask-become-pass --vault-password-file ~/.vault_key -l "workstations" -t "setup" -C
+ansible-playbook playbook.yml -i hosts.yml --ask-become-pass --vault-password-file ~/.vault_key -l "workstations" -t "setup"
