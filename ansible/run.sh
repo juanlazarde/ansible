a1b2bdfe16b5a9c517652319a386a2848763d279
Juan Lazarde <juanlazarde@gmail.com>
2022-02-22 09:03:15 -0500

    config errors

--- ansible/run.sh
+++ ansible/run.sh
@@ -5,18 +5,30 @@
 hosts_available=(servers workstations)
 tags_available=(setup update)
 
+function usage() {
+    printf '%s\n' \
+"Usage:
+
+bash run.sh [host tag] [--deploy-ssh] [--debug] [--step] [--start-at-task task]
+
+bash run.sh
+    Will return list of hosts, tags, and tasks
+
+bash run.sh workstation setup
+    Will run: ansible-playbook playbook.yml --limit workstations --tags setup
+
+"
+}
+
 while [[ "$#" -gt 0 ]]; do
-    for host in "${hosts_available[@]}"; do
-        [[ "${1-}" == "--${host}" || "${1-}" == "${host}" ]] && limit="${host}"
-    done
-    for tag in "${tags_available[@]}"; do
-        [[ "${1-}" == "--${tag}" || "${1-}" == "${tag}" ]] && tags="${tag}"
-    done
+    [[ " ${hosts_available[*]} " =~ " ${1} " ]] && limit="--limit ${1}"
+    [[ " ${tags_available[*]} "  =~ " ${1} " ]] && tags="--tags ${1}"
     case "${1-}" in
-        --deploy-ssh|deploy-ssh)    limit="servers"; tags="ssh" ;;
-        --debug)                    debug="--check -vvv" ;;
-        --step)                     step="--step" ;;
-        --start-at-task)            start_at_task="--start-at-task '${2-}'"; shift ;;
+        --deploy-ssh)       limit="--limit servers"; tags="--tags ssh" ;;
+        --debug)            debug="--check -vvv" ;;
+        --step)             step="--step" ;;
+        --start-at-task)    start_at_task="--start-at-task '${2-}'"; shift ;;
+        --help|-h|/h)       usage; exit 0 ;;
     esac
     shift
 done
@@ -26,9 +38,8 @@ if [[ -n ${limit} && -n ${tags} ]]; then
     set -x
     ansible-playbook ${debug} ${step} ${start_at_task} \
     playbook.yml \
-    --ask-become-pass --vault-password-file ~/.vault_key \
-    --limit "${limit}" \
-    --tags "${tags}"
+    ${limit} \
+    ${tags}
     set +x
 else
     set -x
