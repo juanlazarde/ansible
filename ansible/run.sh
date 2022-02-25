68a01f2c6e8e2cff80d17f2f3446ec778bb28a3d
Juan Lazarde <juanlazarde@gmail.com>
2022-02-25 16:07:43 -0500

    major updates all over

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# USAGE: bash run.sh servers setup
+# USAGE: bash run.sh --debug
+#   bash run.sh servers setup
+#   bash run.sh --deploy-ssh
+#
 
 hosts_available=(servers workstations)
 tags_available=(setup update)
@@ -9,13 +12,21 @@ function usage() {
     printf '%s\n' \
 "Usage:
 
-bash run.sh [host tag] [--deploy-ssh] [--debug] [--step] [--start-at-task task]
+bash run.sh [<HOST> <TAG>] [--deploy-ssh] [--debug] [--step] [--start-at-task TASK] [--args ARGUMENTS] [-h | --help]
 
 bash run.sh
     Will return list of hosts, tags, and tasks
 
-bash run.sh workstation setup
+bash run.sh workstations setup
     Will run: ansible-playbook playbook.yml --limit workstations --tags setup
+
+bash run.sh --deploy-ssh
+    Will run: ansible-playbook deploy_ssh.yml --limit localhost
+              ansible-playbook deploy_ssh.yml --ask-pass --limit 'all:!localhost'
+
+bash run.sh workstations setup --debug --step --start-at-task 'here is a task' --args --ask-pass
+    Will run: ansible-playbook playbook.yml --limit workstations --tags setup --chech -vvv --step
+              --start-at-task 'here is a task' --args --ask-pass
 
 "
 }
@@ -24,9 +35,10 @@ while [[ "$#" -gt 0 ]]; do
     [[ " ${hosts_available[*]} " =~ " ${1} " ]] && limit="--limit ${1}"
     [[ " ${tags_available[*]} "  =~ " ${1} " ]] && tags="--tags ${1}"
     case "${1-}" in
-        --deploy-ssh)       limit="--limit servers"; tags="--tags ssh" ;;
         --debug)            debug="--check -vvv" ;;
         --step)             step="--step" ;;
+        --deploy-ssh)       deploy=true ;;
+        --args)             args="${2-}"; shift ;;
         --start-at-task)    start_at_task="--start-at-task ${2-}"; shift ;;
         --help|-h|/h)       usage; exit 0 ;;
     esac
@@ -36,11 +48,14 @@ done
 clear
 if [[ -n ${limit} && -n ${tags} ]]; then
     set -x
-    ansible-playbook ${debug} ${step} ${start_at_task} \
-    playbook.yml \
-    ${limit} \
-    ${tags}
+    ansible-playbook playbook.yml ${debug} ${step} ${opt} ${start_at_task} ${limit} ${tags} ${args}
     set +x
+elif [[ ${deploy} ]]; then
+    set -x
+    ansible-playbook deploy_ssh.yml --limit localhost ${args}
+    ansible-playbook deploy_ssh.yml --ask-pass --limit "all:!localhost" ${args}
+    set +x
+
 else
     set -x
     ansible-playbook playbook.yml --list-hosts
